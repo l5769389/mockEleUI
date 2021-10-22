7199670675d3138ec0562c195fe7bac7a9c90c3d
JUN LIU <5769389@qq.com>
2021-10-22 16:57:10 +0800

    保存回家

---
+++ eleui/build/bin/gen-cssfile.js
@@ -0,0 +1,6 @@
+const fs =require('fs');
+const path=require('path');
+const ComponentsJson =require('../../components.json');
+
+const Components =Object.keys(ComponentsJson);
+console.log(Components);
